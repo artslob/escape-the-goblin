b9da2f7481d5b58194c37b977f8376853e69f4a5
artslob <artslob@yandex.ru>
2021-03-12 18:53:31 +0300

    add player

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@ const WINDOW_HEIGHT: f32 = 480.0;
 
 struct GameState {
     lake: Lake,
+    player: Player,
+}
+
+struct Player {
+    mesh: Mesh,
+    draw_params: DrawParams,
+}
+
+impl Player {
+    fn new(ctx: &mut Context) -> tetra::Result<Self> {
+        let radius = 5.0;
+        let mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), radius)?;
+        let center = Vec2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
+        let draw_params = DrawParams::new().position(center).color(Color::WHITE);
+        Ok(Self { mesh, draw_params })
+    }
+
+    fn draw(&self, ctx: &mut Context) {
+        self.mesh.draw(ctx, self.draw_params.clone())
+    }
 }
 
 struct Lake {
@@ -18,12 +38,11 @@ struct Lake {
 
 impl Lake {
     fn new(ctx: &mut Context) -> tetra::Result<Self> {
-        let center = Vec2::<f32>::new(0., 0.);
         let radius = WINDOW_HEIGHT / 2.0 * 0.8;
-        let mesh = Mesh::circle(ctx, ShapeStyle::Fill, center, radius)?;
-        let lake_center = Vec2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
+        let mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), radius)?;
+        let position = Vec2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
         let draw_params = DrawParams::new()
-            .position(lake_center)
+            .position(position)
             .color(Color::rgb8(0, 0, 255));
         Ok(Self { mesh, draw_params })
     }
@@ -37,6 +56,7 @@ impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<Self> {
         Ok(Self {
             lake: Lake::new(ctx)?,
+            player: Player::new(ctx)?,
         })
     }
 }
@@ -46,6 +66,7 @@ impl State for GameState {
         graphics::clear(ctx, Color::rgb8(30, 240, 30));
 
         self.lake.draw(ctx);
+        self.player.draw(ctx);
 
         Ok(())
     }
