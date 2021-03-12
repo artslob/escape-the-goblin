4b07ecce3e1d14c123ba0bb9fb0e0524ca1b4013
artslob <artslob@yandex.ru>
2021-03-12 22:45:46 +0300

    add game result

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,15 @@ const WINDOW_HEIGHT: f32 = 480.0;
 
 const PLAYER_SPEED: f32 = 2.0;
 
+#[derive(Eq, PartialEq)]
+enum GameResult {
+    Playing,
+    PlayerWins,
+    GoblinWins,
+}
+
 struct GameState {
+    result: GameResult,
     lake: Lake,
     player: Player,
 }
@@ -46,11 +54,9 @@ struct Lake {
 
 impl Lake {
     fn new(ctx: &mut Context) -> tetra::Result<Self> {
-        let radius = WINDOW_HEIGHT / 2.0 * 0.8;
-        let mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), radius)?;
-        let position = Vec2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
+        let mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), Self::radius())?;
         let draw_params = DrawParams::new()
-            .position(position)
+            .position(Self::center())
             .color(Color::rgb8(0, 0, 255));
         Ok(Self { mesh, draw_params })
     }
@@ -58,11 +64,20 @@ impl Lake {
     fn draw(&self, ctx: &mut Context) {
         self.mesh.draw(ctx, self.draw_params.clone())
     }
+
+    fn center() -> Vec2<f32> {
+        Vec2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0)
+    }
+
+    fn radius() -> f32 {
+        WINDOW_HEIGHT / 2.0 * 0.8
+    }
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<Self> {
         Ok(Self {
+            result: GameResult::Playing,
             lake: Lake::new(ctx)?,
             player: Player::new(ctx)?,
         })
@@ -71,6 +86,10 @@ impl GameState {
 
 impl State for GameState {
     fn update(&mut self, ctx: &mut Context) -> Result<(), TetraError> {
+        if self.result != GameResult::Playing {
+            return Ok(());
+        }
+
         let y = if input::is_key_down(ctx, Key::W) {
             Some(-PLAYER_SPEED)
         } else if input::is_key_down(ctx, Key::S) {
@@ -96,15 +115,31 @@ impl State for GameState {
         };
         self.player.position += player_move;
 
+        if (self.player.position.x - Lake::center().x).powi(2)
+            + (self.player.position.y - Lake::center().y).powi(2)
+            > Lake::radius().powi(2)
+        {
+            self.result = GameResult::PlayerWins;
+        }
+
         // TODO check player is inside lake?
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> Result<(), TetraError> {
-        graphics::clear(ctx, Color::rgb8(30, 240, 30));
-
-        self.lake.draw(ctx);
-        self.player.draw(ctx);
+        match self.result {
+            GameResult::Playing => {
+                graphics::clear(ctx, Color::rgb8(30, 240, 30));
+                self.lake.draw(ctx);
+                self.player.draw(ctx);
+            }
+            GameResult::PlayerWins => {
+                graphics::clear(ctx, Color::rgb8(30, 144, 255));
+            }
+            GameResult::GoblinWins => {
+                graphics::clear(ctx, Color::rgb8(240, 30, 30));
+            }
+        }
 
         Ok(())
     }
