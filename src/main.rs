555331c912a3361836aad6977631a61f178713a0
artslob <artslob@yandex.ru>
2021-03-12 16:20:25 +0300

    tetra hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use graphics::Color;
+use tetra::{graphics, Context, ContextBuilder, State, TetraError};
+
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 480.0;
+
+struct GameState {}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), TetraError> {
+        graphics::clear(ctx, Color::rgb8(30, 240, 30));
+        Ok(())
+    }
+}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new(
+        "Escape the Goblin!",
+        WINDOW_WIDTH as i32,
+        WINDOW_HEIGHT as i32,
+    )
+    .build()?
+    .run(|_| Ok(GameState {}))
 }
