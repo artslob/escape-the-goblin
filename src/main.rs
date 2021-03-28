d6a6d9f74982394759b9c8a4fef6888f3106c47d
artslob <artslob@yandex.ru>
2021-03-29 00:23:24 +0300

    update radius and speed of player and goblin

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,8 @@ use tetra::{graphics, input, Context, ContextBuilder, Event, State, TetraError};
 
 const PI: f32 = std::f32::consts::PI;
 
-const PLAYER_SPEED: f32 = 2.0;
-const GOBLIN_SPEED: f32 = PLAYER_SPEED * 4.0;
-
-// TODO allow resize window
+// goblin is faster than player
+const SPEED_RATIO: f32 = 4.0;
 
 struct HelpingCircle {
     mesh: Mesh,
@@ -21,7 +19,7 @@ struct HelpingCircle {
 
 impl HelpingCircle {
     fn new(ctx: &mut Context, window: &Window, lake: &Lake) -> tetra::Result<Self> {
-        let radius = lake.radius / (GOBLIN_SPEED / PLAYER_SPEED);
+        let radius = lake.radius / SPEED_RATIO;
         let mesh = Mesh::circle(ctx, ShapeStyle::Stroke(1.0), Vec2::zero(), radius)?;
         let position = window.center();
         Ok(Self {
@@ -44,7 +42,7 @@ impl HelpingCircle {
         window: &Window,
         lake: &Lake,
     ) -> tetra::Result {
-        self.radius = lake.radius / (GOBLIN_SPEED / PLAYER_SPEED);
+        self.radius = lake.radius / SPEED_RATIO;
         self.position = window.center();
         self.mesh = Mesh::circle(ctx, ShapeStyle::Stroke(1.0), Vec2::zero(), self.radius)?;
         Ok(())
@@ -55,10 +53,16 @@ struct Goblin {
     mesh: Mesh,
     position: Vec2<f32>,
     radius: f32,
+    speed: f32,
 }
 
 impl Goblin {
-    fn new(ctx: &mut Context, window: &Window, lake: &Lake) -> tetra::Result<Self> {
+    fn new(
+        ctx: &mut Context,
+        window: &Window,
+        lake: &Lake,
+        player: &Player,
+    ) -> tetra::Result<Self> {
         let radius = 5.0;
         let mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), radius)?;
         let center = Vec2::new(window.width / 2.0, window.center().y - lake.radius);
@@ -66,6 +70,7 @@ impl Goblin {
             mesh,
             radius,
             position: center,
+            speed: player.speed * SPEED_RATIO,
         })
     }
 
@@ -73,12 +78,27 @@ impl Goblin {
         let draw_params = DrawParams::new().position(self.position).color(Color::RED);
         self.mesh.draw(ctx, draw_params);
     }
+
+    fn on_window_resize(
+        &mut self,
+        ctx: &mut Context,
+        window: &Window,
+        previous_window: &Window,
+    ) -> tetra::Result {
+        let ratio = window.height / previous_window.height;
+        self.speed *= ratio;
+        self.radius *= ratio;
+        self.mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), self.radius)?;
+        // TODO calc new position
+        Ok(())
+    }
 }
 
 struct Player {
     mesh: Mesh,
     position: Vec2<f32>,
     radius: f32,
+    speed: f32,
 }
 
 impl Player {
@@ -89,6 +109,7 @@ impl Player {
             mesh,
             radius,
             position: window.center(),
+            speed: 2.0,
         })
     }
 
@@ -105,10 +126,12 @@ impl Player {
         window: &Window,
         previous_window: &Window,
     ) -> tetra::Result {
+        let ratio = window.height / previous_window.height;
+        self.speed *= ratio;
+        self.radius *= ratio;
         self.mesh = Mesh::circle(ctx, ShapeStyle::Fill, Vec2::zero(), self.radius)?;
         let vector = self.position - previous_window.center();
         // new position depends on heights ratio because radius of lake depends on height
-        let ratio = window.height / previous_window.height;
         self.position = window.center() + vector * ratio;
         Ok(())
     }
@@ -166,8 +189,8 @@ impl Window {
 impl Default for Window {
     fn default() -> Self {
         Self {
-            width: 1280.,
-            height: 720.,
+            width: 640.,
+            height: 480.,
         }
     }
 }
@@ -190,8 +213,8 @@ impl GameState {
         let player_wins_text = Text::new("You win! Congrats!", font.clone());
         let goblin_wins_text = Text::new("Goblin wins!", font.clone());
         let lake = Lake::new(ctx, &window)?;
-        let goblin = Goblin::new(ctx, &window, &lake)?;
         let player = Player::new(ctx, &window)?;
+        let goblin = Goblin::new(ctx, &window, &lake, &player)?;
         let helping_circle = HelpingCircle::new(ctx, &window, &lake)?;
         Ok(Self {
             window,
@@ -213,22 +236,22 @@ impl State for GameState {
         }
 
         let y = if input::is_key_down(ctx, Key::W) {
-            Some(-PLAYER_SPEED)
+            Some(-self.player.speed)
         } else if input::is_key_down(ctx, Key::S) {
-            Some(PLAYER_SPEED)
+            Some(self.player.speed)
         } else {
             None
         };
         let x = if input::is_key_down(ctx, Key::A) {
-            Some(-PLAYER_SPEED)
+            Some(-self.player.speed)
         } else if input::is_key_down(ctx, Key::D) {
-            Some(PLAYER_SPEED)
+            Some(self.player.speed)
         } else {
             None
         };
         let player_move = match (x, y) {
             (Some(x), Some(y)) => {
-                let value = (PLAYER_SPEED.powi(2) / 2.0).sqrt();
+                let value = (self.player.speed.powi(2) / 2.0).sqrt();
                 Vec2::new(value * x.signum(), value * y.signum())
             }
             (Some(x), None) => Vec2::new(x, 0.0),
@@ -242,8 +265,8 @@ impl State for GameState {
             let mouse_position = input::get_mouse_position(ctx);
             let difference = mouse_position - self.player.position;
             let magnitude = difference.magnitude();
-            let position = if magnitude > PLAYER_SPEED {
-                self.player.position + difference * PLAYER_SPEED / magnitude
+            let position = if magnitude > self.player.speed {
+                self.player.position + difference * self.player.speed / magnitude
             } else {
                 mouse_position
             };
@@ -253,19 +276,19 @@ impl State for GameState {
         let center = self.window.center();
 
         // 1. find arc length
-        // 2. if length < GOBLIN_SPEED, move goblin to player
-        // 3. else move goblin to GOBLIN_SPEED point
+        // 2. if length < self.goblin.speed, move goblin to player
+        // 3. else move goblin to self.goblin.speed point
         let goblin_vector = self.goblin.position - center;
         let player_vector = self.player.position - center;
         let angle = goblin_vector.angle_between(player_vector); // angle in radians
 
         // https://tutors.com/math-tutors/geometry-help/how-to-find-arc-measure-formula
         let arc_length = self.lake.radius * angle;
-        if arc_length <= GOBLIN_SPEED {
+        if arc_length <= self.goblin.speed {
             self.goblin.position =
                 center + player_vector * (self.lake.radius / player_vector.magnitude());
         } else {
-            let arc_length = GOBLIN_SPEED;
+            let arc_length = self.goblin.speed;
             let angle = arc_length / self.lake.radius;
             // https://www.euclideanspace.com/maths/algebra/vectors/angleBetween/
             // https://stackoverflow.com/questions/21483999/using-atan2-to-find-angle-between-two-vectors
@@ -344,10 +367,13 @@ impl State for GameState {
             self.window.width = width as f32;
             self.window.height = height as f32;
             self.lake.on_window_resize(ctx, &self.window)?;
-            self.helping_circle
-                .on_window_resize(ctx, &self.window, &self.lake)?;
             self.player
                 .on_window_resize(ctx, &self.window, &previous_window)?;
+            self.goblin
+                .on_window_resize(ctx, &self.window, &previous_window)?;
+            self.helping_circle
+                .on_window_resize(ctx, &self.window, &self.lake)?;
+            self.update(ctx)?;
             // println!("{} {}", width, height);
         }
         Ok(())
@@ -363,7 +389,7 @@ fn main() -> tetra::Result {
     )
     .title("Escape the Goblin!")
     .show_mouse(true)
-    // .resizable(true)
+    .resizable(true)
     .build()?
     .run(|ctx| GameState::new(ctx, window))
 }
