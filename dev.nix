b0926cad565ff8c2d9fc3d807b42bf224816fbb1
artslob <artslob@yandex.ru>
2023-01-15 18:30:22 +0700

    add nix shell

---
+++ dev.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz")
+  { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.git
+    pkgs.pre-commit
+    pkgs.nixfmt
+    pkgs.cargo
+    pkgs.pkg-config # required to find alsa in c code
+    pkgs.alsa-lib
+    pkgs.SDL2
+  ];
+}
