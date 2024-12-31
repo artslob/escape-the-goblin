37579f1de40ec9e8c99f5ab3619ce9f90d9455b7
artslob <artslob@yandex.ru>
2024-12-31 13:12:07 +0700

    update nix-shell for development

--- dev.nix
+++ dev.nix
@@ -1,19 +1,42 @@
-{ pkgs ? import (fetchTarball
-  "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz")
+{ pkgs ? import
+  (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz")
   { } }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.git
-    pkgs.pre-commit
-    pkgs.nixfmt
-    pkgs.cargo
-    pkgs.rustc
-    pkgs.rustup
-    pkgs.pkg-config # required to find alsa in c code
-    pkgs.alsa-lib
-    pkgs.SDL2
+pkgs.mkShell rec {
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+
+  buildInputs = with pkgs; [
+    git
+    pre-commit
+    nixfmt-classic
+    cargo
+    rustc
+    rustup
+    # glibc
+    # pkg-config # required to find alsa in c code
+    # alsa-lib
+    # alsa-lib.dev
+    # udev.dev
+    # alsa-lib.dev
+    alsa-lib-with-plugins
+    # alsa-oss
+    # alsa-utils
+    # alsa-tools
+    # alsa-plugins
+    # alsa-ucm-conf
+    # alsa-firmware
+    # alsa-topology-conf
+    # pulseaudio.dev
+    # pavucontrol
+    SDL2
+    # glxinfo
+    # mesa
+    mesa.drivers
+    # libGL
+    # vulkan-tools
   ];
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
 
   # required for codium rust-analyzer to work
   # https://discourse.nixos.org/t/11570
